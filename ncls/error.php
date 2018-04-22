ccd0ffd882e960f5e65373b2922d63be9ad31ff4
Glitch (jpn-food-paris) <none>
2018-04-22 14:27:16 +0000

    🐸🎅 Checkpoint ./css/style.css:826745/280 ./ncls/error.php:826745/833

--- ncls/error.php
+++ ncls/error.php
@@ -9,8 +9,26 @@
     }
     
   }
+
+
+  if(isset($error)){
+    
+    if($error=="404"){
+      
+      $corpus = "we couldn't find the page you are looking for<br>maybe this page has been erased or the url is wrong.";
+      
+    }
+    if($error=="403"){
+      
+      $corpus = "sorry, you are not able to access this page.";
+      
+    }
+    
+    
+  }
   
   echo '<link rel="stylesheet" type="text/css" href="/css/style.css">';
-  echo "<p class='e'>
-          <h1 class='error'>".$error."</h1>
-        </p>";
+  echo '<div class="err">
+          <h1 class="error">'.$error.'</h1>
+          '.$corpus.'
+        </div>';
