11e0f26dd846577a15618e090938756c66b7ea61
Glitch (jpn-food-paris) <none>
2018-04-22 14:37:35 +0000

    🏎👳 Checkpoint ./ncls/header.php:826745/228 ./css/style.css:826745/310

--- ncls/header.php
+++ ncls/header.php
@@ -6,3 +6,13 @@
   JPN/食べ物/Par!s
   </h1>
 </header>
+<table>
+  <tr>
+    <th>
+      <a href="?page=corpus">home</a>
+    </th>
+    <th>
+      <a href="?page=search">search</a>
+    </th>
+  </tr>
+</table>
