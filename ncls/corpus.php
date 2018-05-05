4941c82f29d6f54e6b96c4cbdc7ea7392b416c6c
Glitch (php-html-lamp-server) <none>
2018-05-05 05:29:17 +0000

    🐿⛪️ Checkpoint ./ncls/corpus.php:826745/957 ./css/style.css:826745/60

---
+++ ncls/corpus.php
@@ -0,0 +1,5 @@
+<div class="container"><img src="https://s24255.pcdn.co/wp-content/uploads/2017/05/lamp_stack.jpg" width="250">
+
+"LAMP is an archetypal model of web service stacks, named as an acronym of the names of its original four open-source components: the Linux operating system, the Apache HTTP Server, the MySQL relational database management system (RDBMS), and the PHP programming language. The LAMP components are largely interchangeable and not limited to the original selection. As a solution stack, LAMP is suitable for building dynamic web sites and web applications.[1]
+
+Since its creation, the LAMP model has been adapted to other componentry, though typically consisting of free and open-source software. For example, an equivalent installation on the Microsoft Windows family of operating systems is known as WAMP and an equivalent installation on macOS is known as MAMP." (from wikipedia)</div>
